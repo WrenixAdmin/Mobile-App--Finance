8eae92324990524d84e65ed045e17b31ed8425a1
Tuddeepana <tauddeepana@gmail.com>
2025-02-28 08:29:34 +0530

    add the styles

--- FrontEnd/finance/lib/utils/colors.dart
+++ FrontEnd/finance/lib/utils/colors.dart
@@ -6,6 +6,7 @@ class AppColors {
 
   static const Color background = Color(0xFFE5E5F5);
   static const Color primaryColor = Color(0xFF5643F4);
+  static const Color secondaryColor = Color(0xFFEFFFFF);
   static const Color buttonBackground = Colors.blueAccent;
   static const Color textPrimary = Colors.indigo;
   static const Color textSecondary = Colors.grey;
