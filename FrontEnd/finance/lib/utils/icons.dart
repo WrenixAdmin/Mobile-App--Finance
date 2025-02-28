a655847f987cdbabd4b792653943cf581cceecd1
Tuddeepana <129895353+Tuddeepana@users.noreply.github.com>
2025-03-01 00:35:56 +0530

    Revert "screen added"

--- FrontEnd/finance/lib/utils/icons.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppIcons {
-  static const IconData activeIndicator = Icons.circle;
-  static const IconData inactiveIndicator = Icons.circle;
-  static const IconData receipt = Icons.receipt;
-  static const IconData calendar = Icons.calendar_today;
-}
